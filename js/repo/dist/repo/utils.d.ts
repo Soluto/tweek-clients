b728570a2dd57f4d9436eb31d4e70bbce9c5a01c
yshay <yshayy@gmail.com>
2017-01-19 17:58:11 +0200

    move packages

--- js/repo/dist/repo/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function partitionByIndex(arr: any, index: any): any;
-export declare function distinct(arr: any): {}[];
-export declare function snakeToCamelCase(keyName: any): string;
