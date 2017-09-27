3594b478279311940499e98e32716a875a663943
Ore Poran <oreporan@gmail.com>
2017-09-27 18:21:53 +0300

    allow more types in identityContext (#53)
    * add types reference in package.json of tweek-client
    
    * add typings decleration to tweek-local-cache
    
    * change context property to accept number | boolean | string

--- js/tweek-client/index.ts
+++ js/tweek-client/index.ts
@@ -1,181 +1,192 @@
 import * as queryString from 'query-string';
 import 'isomorphic-fetch';
 
-export type IdentityContext = { id?: string; } & {
-    [prop: string]: string;
-}
+export type IdentityContext = { id?: string } & {
+  [prop: string]: string | number | boolean;
+};
 
 export type Context = {
-    [identityType: string]: IdentityContext
-}
+  [identityType: string]: IdentityContext;
+};
 
-export type TweekCasing = "snake" | "camelCase";
+export type TweekCasing = 'snake' | 'camelCase';
 
 export type FetchConfig = {
-    include?: string[],
-    casing?: TweekCasing;
-    convertTyping?: boolean;
-    flatten?: boolean;
-    context?: Context;
-}
+  include?: string[];
+  casing?: TweekCasing;
+  convertTyping?: boolean;
+  flatten?: boolean;
+  context?: Context;
+};
 
 export type TweekInitConfig = FetchConfig & {
-    baseServiceUrl: string;
-    fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
-}
+  baseServiceUrl: string;
+  fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+};
 
 function captialize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function snakeToCamelCase(target) {
-    if (typeof (target) !== "object") return target;
-    return Object.keys(target).reduce((o, key) => {
-        let [firstKey, ...others] = key.split("_");
-        let newKey = [firstKey, ...others.map(captialize)].join("");
-        o[newKey] = snakeToCamelCase(target[key]);
-        return o;
-    }, {});
+  if (typeof target !== 'object') return target;
+  return Object.keys(target).reduce((o, key) => {
+    let [firstKey, ...others] = key.split('_');
+    let newKey = [firstKey, ...others.map(captialize)].join('');
+    o[newKey] = snakeToCamelCase(target[key]);
+    return o;
+  }, {});
 }
 
 function convertTypingFromJSON(target) {
-    if (typeof (target) === "string") {
-        try {
-            return JSON.parse(target);
-        } catch (e) {
-            return target;
-        }
+  if (typeof target === 'string') {
+    try {
+      return JSON.parse(target);
+    } catch (e) {
+      return target;
     }
-    else if (typeof (target) === "object") {
-        return Object.keys(target).reduce((o, key) => {
-            o[key] = convertTypingFromJSON(target[key]);
-            return o;
-        }, {});
-    }
-    else return target;
+  } else if (typeof target === 'object') {
+    return Object.keys(target).reduce((o, key) => {
+      o[key] = convertTypingFromJSON(target[key]);
+      return o;
+    }, {});
+  } else return target;
 }
 
 export interface ITweekClient {
-    fetch<T>(path: string, config?: FetchConfig): Promise<T>;
-    appendContext(identityType: string, identityId: string, context: object): Promise<void>;
-    deleteContext(identityType: string, identityId: string, property: string): Promise<void>;
+  fetch<T>(path: string, config?: FetchConfig): Promise<T>;
+  appendContext(identityType: string, identityId: string, context: object): Promise<void>;
+  deleteContext(identityType: string, identityId: string, property: string): Promise<void>;
 }
 
 export class TweekClient implements ITweekClient {
-    config: TweekInitConfig;
+  config: TweekInitConfig;
 
-    private static ENCODE_$_CHARACTER = encodeURIComponent('$');
-    private static ENCODE_SLASH_CHARACTER = encodeURIComponent('/');
+  private static ENCODE_$_CHARACTER = encodeURIComponent('$');
+  private static ENCODE_SLASH_CHARACTER = encodeURIComponent('/');
 
-    constructor(config: TweekInitConfig) {
-        this.config = <TweekInitConfig & FetchConfig>
-            { ...{ casing: "snake", flatten: false, convertTyping: false, context: {} }, ...config };
+  constructor(config: TweekInitConfig) {
+    this.config = <TweekInitConfig & FetchConfig>{
+      ...{ casing: 'snake', flatten: false, convertTyping: false, context: {} },
+      ...config,
+    };
 
-        let {baseServiceUrl} = config;
+    let { baseServiceUrl } = config;
 
-        if (baseServiceUrl.endsWith('/')) {
-            baseServiceUrl = baseServiceUrl.substr(0, baseServiceUrl.length -1);
-            this.config.baseServiceUrl = baseServiceUrl;
+    if (baseServiceUrl.endsWith('/')) {
+      baseServiceUrl = baseServiceUrl.substr(0, baseServiceUrl.length - 1);
+      this.config.baseServiceUrl = baseServiceUrl;
+    }
+  }
+
+  fetch<T>(path: string, _config?: FetchConfig): Promise<T> {
+    const { casing, flatten, baseServiceUrl, convertTyping, context, include } = <TweekInitConfig & FetchConfig>{
+      ...this.config,
+      ..._config,
+    };
+    let queryParamsObject = this._contextToQueryParams(context);
+
+    if (flatten) {
+      queryParamsObject['$flatten'] = true;
+    }
+
+    queryParamsObject['$include'] = include;
+    let queryParams = queryString.stringify(queryParamsObject);
+    queryParams = this.queryParamsEncoder(queryParams);
+
+    const url =
+      baseServiceUrl +
+      '/api/v1/keys' +
+      (path.startsWith('/') ? '' : '/') +
+      path +
+      (!!queryParams ? `?${queryParams}` : '');
+    let result: Promise<any> = this.config.fetch(url).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(new Error(response.statusText));
+      }
+    });
+
+    if (!flatten && casing === 'camelCase') {
+      result = result.then(snakeToCamelCase);
+    }
+
+    if (convertTyping) {
+      result = result.then(convertTypingFromJSON);
+    }
+
+    return <Promise<T>>result;
+  }
+
+  appendContext(identityType: string, identityId: string, context: object): Promise<void> {
+    const url = `${this.config.baseServiceUrl}/api/v1/context/${identityType}/${identityId}`;
+    let result = this.config
+      .fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(context),
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error appending context, code ${response.status}, message: '${response.statusText}'`);
         }
-    }
+      });
+    return <Promise<void>>result;
+  }
 
-    fetch<T>(path: string, _config?: FetchConfig): Promise<T> {
-        const { casing, flatten, baseServiceUrl, convertTyping, context, include } =
-            <TweekInitConfig & FetchConfig>{ ...this.config, ..._config };
-        let queryParamsObject = this._contextToQueryParams(context);
+  deleteContext(identityType: string, identityId: string, property: string): Promise<void> {
+    const url = `${this.config.baseServiceUrl}/api/v1/context/${identityType}/${identityId}/${property}`;
+    let result = this.config.fetch(url, { method: 'DELETE' }).then(response => {
+      if (!response.ok) {
+        throw new Error(`Error deleting context property, code ${response.status}, message: '${response.statusText}'`);
+      }
+    });
+    return <Promise<void>>result;
+  }
 
-        if (flatten) {
-            queryParamsObject['$flatten'] = true;
-        }
+  public queryParamsEncoder = (queryParams: string) =>
+    queryParams.replace(/\$/g, TweekClient.ENCODE_$_CHARACTER).replace(/\//g, TweekClient.ENCODE_SLASH_CHARACTER);
 
-        queryParamsObject['$include'] = include;
-        let queryParams = queryString.stringify(queryParamsObject);
-        queryParams = this.queryParamsEncoder(queryParams);
-
-        const url = baseServiceUrl + '/api/v1/keys' + (path.startsWith('/') ? '' : '/') + path + (!!queryParams ? `?${queryParams}` : '');
-        let result: Promise<any> = this.config.fetch(url)
-            .then(response => {
-                if (response.ok) {
-                    return response.json()
-                }
-                else {
-                    return Promise.reject(new Error(response.statusText));
-                }
-            });
-
-        if (!flatten && casing === "camelCase") {
-            result = result.then(snakeToCamelCase);
-        }
-
-        if (convertTyping) {
-            result = result.then(convertTypingFromJSON);
-        }
-
-        return <Promise<T>>result;
-    }
-
-    appendContext(identityType: string, identityId: string, context: object): Promise<void> {
-        const url = `${this.config.baseServiceUrl}/api/v1/context/${identityType}/${identityId}`;
-        let result = this.config.fetch(url, { method: 'POST', 
-                                             headers: {
-                                                'Content-Type': 'application/json'
-                                             },
-                                             body: JSON.stringify(context) })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error appending context, code ${response.status}, message: '${response.statusText}'`);
-                }
-            });
-        return <Promise<void>>result;
-    }
-
-    deleteContext(identityType: string, identityId: string, property: string): Promise<void> {
-        const url = `${this.config.baseServiceUrl}/api/v1/context/${identityType}/${identityId}/${property}`;
-        let result = this.config.fetch(url, { method: 'DELETE' })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error deleting context property, code ${response.status}, message: '${response.statusText}'`);
-                }
-            });
-        return <Promise<void>>result;
-    }
-
-    public queryParamsEncoder = (queryParams: string) => queryParams
-        .replace(/\$/g, TweekClient.ENCODE_$_CHARACTER)
-        .replace(/\//g, TweekClient.ENCODE_SLASH_CHARACTER);
-
-    private _contextToQueryParams = context => {
-        return Object.keys(context).reduce((pre, cur) => {
-            let identityContext = context[cur];
-            Object.keys(identityContext).forEach(x =>
-                x === 'id' ? pre[`${cur}`] = identityContext[x] : pre[`${cur}.${x}`] = identityContext[x]);
-            return pre;
-        }, {});
-    }
+  private _contextToQueryParams = context => {
+    return Object.keys(context).reduce((pre, cur) => {
+      let identityContext = context[cur];
+      Object.keys(identityContext).forEach(
+        x => (x === 'id' ? (pre[`${cur}`] = identityContext[x]) : (pre[`${cur}.${x}`] = identityContext[x])),
+      );
+      return pre;
+    }, {});
+  };
 }
 
-export function createTweekClient(config: { baseServiceUrl: string, context?: any, getAuthenticationToken?: () => Promise<string> | string }) {
-    const { baseServiceUrl, context = {}, getAuthenticationToken } = config;
+export function createTweekClient(config: {
+  baseServiceUrl: string;
+  context?: any;
+  getAuthenticationToken?: () => Promise<string> | string;
+}) {
+  const { baseServiceUrl, context = {}, getAuthenticationToken } = config;
 
-    let fetchClient = (input, init)=>fetch(input, init);
-    if (getAuthenticationToken) {
-        fetchClient = async (input, init = {})=> {
-            const token = await Promise.resolve(getAuthenticationToken());
-            return fetch(input, {
-                ...init,
-                headers: {
-                    ...init.headers,
-                    Authorization: `Bearer ${token}`
-                }
-            });
-        }
-    }
+  let fetchClient = (input, init) => fetch(input, init);
+  if (getAuthenticationToken) {
+    fetchClient = async (input, init = {}) => {
+      const token = await Promise.resolve(getAuthenticationToken());
+      return fetch(input, {
+        ...init,
+        headers: {
+          ...init.headers,
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    };
+  }
 
-    return new TweekClient({
-        baseServiceUrl,
-        casing: "camelCase",
-        convertTyping: false,
-        context,
-        fetch: fetchClient,
-    });
+  return new TweekClient({
+    baseServiceUrl,
+    casing: 'camelCase',
+    convertTyping: false,
+    context,
+    fetch: fetchClient,
+  });
 }
