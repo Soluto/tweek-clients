645585bc514e34af65b166badc920ac3e803453a
Nataly Shrits <nataly87s@gmail.com>
2017-10-09 18:49:35 +0300

    auto refresh config

--- js/clients-example/src/index.js
+++ js/clients-example/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-tweek';
+import { createTweekClient, watchVersion } from 'tweek-client';
+import TweekLocalCache from 'tweek-local-cache';
 import ThemeProvider from './ThemeProvider';
 import App from './App';
 import './index.css';
 
+const baseServiceUrl = 'http://localhost:1111';
+
+const tweekClient = createTweekClient({ baseServiceUrl });
+const repo = new TweekLocalCache({ client: tweekClient });
+
+const watcher = watchVersion(baseServiceUrl);
+watcher.subscribe(() => repo.refresh());
+
 ReactDOM.render(
-  <Provider baseServiceUrl="http://localhost:1111" refresh>
+  <Provider repo={repo}>
     <ThemeProvider>
       <App />
     </ThemeProvider>
