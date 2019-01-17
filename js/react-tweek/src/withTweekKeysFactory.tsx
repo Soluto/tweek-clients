46f8cad60106c33cd0de1b4c6b3612289b3b0e60
Nataly Shrits <nataly87s@gmail.com>
2019-01-17 16:51:31 +0200

    prettier tsx files

--- js/react-tweek/src/withTweekKeysFactory.tsx
+++ js/react-tweek/src/withTweekKeysFactory.tsx
@@ -7,11 +7,15 @@ import { ComponentEnhancer } from './typeUtils';
 export type WithTweekKeysOptions = {
   onError?: (error: Error) => void;
   getPolicy?: GetPolicy;
-}
+};
 
 export interface WithTweekKeys {
-  (keyPropsMapping: {[s: string]: string}, options?: WithTweekKeysOptions): (BaseComponent: ComponentType<any>) => ComponentType<any>
-  <TTweekProps>(keyPropsMapping: Record<keyof TTweekProps, string>, options?: WithTweekKeysOptions): ComponentEnhancer<TTweekProps>
+  (keyPropsMapping: { [s: string]: string }, options?: WithTweekKeysOptions): (
+    BaseComponent: ComponentType<any>,
+  ) => ComponentType<any>;
+  <TTweekProps>(keyPropsMapping: Record<keyof TTweekProps, string>, options?: WithTweekKeysOptions): ComponentEnhancer<
+    TTweekProps
+  >;
 }
 
 type WithTweekKeysState = { [s: string]: any };
@@ -36,7 +40,9 @@ export class WithTweekKeysComponent extends Component<WithTweekKeysRepoProps, Wi
   componentDidUpdate(prevProps: WithTweekKeysRepoProps) {
     if (prevProps._tweekRepo !== this.props._tweekRepo) {
       this._unsubscribe();
-      this.setState(Object.keys(this.props._keyPropsMapping).reduce((acc, prop) => ({...acc, [prop]: undefined}), {}));
+      this.setState(
+        Object.keys(this.props._keyPropsMapping).reduce((acc, prop) => ({ ...acc, [prop]: undefined }), {}),
+      );
       this._subscribeToKeys();
     }
   }
@@ -50,7 +56,7 @@ export class WithTweekKeysComponent extends Component<WithTweekKeysRepoProps, Wi
       const isScanKey = path.split('/').pop() === '_';
       return this.props._tweekRepo.observe(path, this.props._getPolicy).subscribe(
         result => {
-          this.setState((state) => {
+          this.setState(state => {
             if (!isScanKey) {
               result = result.hasValue ? result.value : null;
             }
@@ -72,10 +78,10 @@ export class WithTweekKeysComponent extends Component<WithTweekKeysRepoProps, Wi
   private _unsubscribe() {
     this._subscriptions.forEach(x => x.unsubscribe());
     this._subscriptions = [];
-  };
+  }
 
   private _shouldRender() {
-    return Object.keys(this.props._keyPropsMapping).every((key) => this.state[key] !== undefined);
+    return Object.keys(this.props._keyPropsMapping).every(key => this.state[key] !== undefined);
   }
 
   render() {
@@ -84,16 +90,18 @@ export class WithTweekKeysComponent extends Component<WithTweekKeysRepoProps, Wi
       return null;
     }
 
-    const {_baseComponent: BaseComponent} = this.props;
-    return <BaseComponent {...this.state} {...omit(this.props, '_tweekRepo', '_keyPropsMapping', '_onError', '_getPolicy', '_baseComponent')} />;
+    const { _baseComponent: BaseComponent } = this.props;
+    return (
+      <BaseComponent
+        {...this.state}
+        {...omit(this.props, '_tweekRepo', '_keyPropsMapping', '_onError', '_getPolicy', '_baseComponent')}
+      />
+    );
   }
 }
 
 export default (TweekContext: Context<TweekRepository>, prepare: (key: string) => void): WithTweekKeys =>
-  function (
-    keyPropsMapping: {[s: string]: string},
-    { getPolicy, onError }: WithTweekKeysOptions = {}
-  ) {
+  function(keyPropsMapping: { [s: string]: string }, { getPolicy, onError }: WithTweekKeysOptions = {}) {
     if (!getPolicy || getPolicy.notPrepared === NotPreparedPolicy.prepare) {
       Object.values(keyPropsMapping).forEach(key => prepare(key));
     }
@@ -101,8 +109,17 @@ export default (TweekContext: Context<TweekRepository>, prepare: (key: string) =
     return (BaseComponent: ComponentType<any>) => {
       return (props: {}) => (
         <TweekContext.Consumer>
-          {(repo) => <WithTweekKeysComponent _tweekRepo={repo} _baseComponent={BaseComponent} _onError={onError} _getPolicy={getPolicy} _keyPropsMapping={keyPropsMapping} {...props}/>}
+          {repo => (
+            <WithTweekKeysComponent
+              _tweekRepo={repo}
+              _baseComponent={BaseComponent}
+              _onError={onError}
+              _getPolicy={getPolicy}
+              _keyPropsMapping={keyPropsMapping}
+              {...props}
+            />
+          )}
         </TweekContext.Consumer>
       );
     };
-  }
+  };
