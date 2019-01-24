accb6b629b69216f5eb6dde0bbc8146470d0e9ab
Nataly Shrits <nataly87s@gmail.com>
2019-01-24 19:10:36 +0200

    [tweek-client] add createTweekManagementClient (#126)
    * createTweekManagementClient
    
    * bump version

--- js/tweek-client/src/index.ts
+++ js/tweek-client/src/index.ts
@@ -2,5 +2,6 @@ export * from './types';
 export * from './TweekClient';
 export * from './TweekManagementClient';
 export * from './createTweekClient';
+export * from './createTweekManagementClient';
 export * from './VersionWatcher';
 export * from './FetchError';
--- js/tweek-client/src/types.ts
+++ js/tweek-client/src/types.ts
@@ -6,3 +6,15 @@ export type TweekInitConfig = {
   baseServiceUrl: string;
   fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
 };
+
+export type FetchClientConfig = {
+  getAuthenticationToken?: () => Promise<string> | string;
+  clientName?: string;
+  fetch?: typeof fetch;
+  requestTimeoutInMillis?: number;
+  onError?(response: Response): void;
+};
+
+export type CreateClientConfig = FetchClientConfig & {
+  baseServiceUrl: string;
+};
