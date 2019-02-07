2c1e997fa1d19cd4bf2f2a92b5d2f806b0b5bb4a
Nataly Shrits <nataly87s@gmail.com>
2019-02-07 18:19:59 +0200

    [tweek-client] fix getKeyDependents types (#138)
    * fix getKeyDependents types
    
    * rename
    
    * bump version

--- js/tweek-client/src/TweekManagementClient/index.ts
+++ js/tweek-client/src/TweekManagementClient/index.ts
@@ -5,12 +5,13 @@ import {
   CurrentUser,
   ITweekManagementClient,
   KeyDefinition,
+  KeyDependents,
   KeyManifest,
   Patch,
+  Policy,
   Revision,
   Schema,
   Services,
-  Policy,
 } from './types';
 import { InputParams } from 'query-string';
 import { FetchError } from '../FetchError';
@@ -38,7 +39,7 @@ export class TweekManagementClient implements ITweekManagementClient {
     return this._fetch(url).then(toJson);
   }
 
-  getKeyDependents(path: string): Promise<string[]> {
+  getKeyDependents(path: string): Promise<KeyDependents> {
     const url = `${this.config.baseServiceUrl}/api/v2/dependents/${path}`;
     return this._fetch(url).then(toJson);
   }
@@ -194,7 +195,7 @@ export class TweekManagementClient implements ITweekManagementClient {
       .then(x => x.data);
   }
 
-  updateJWTExtractionPolicy(jwtRegoPolicy: string): Promise<void> {
+  saveJWTExtractionPolicy(jwtRegoPolicy: string): Promise<void> {
     const url = `${this.config.baseServiceUrl}/api/v2/jwt-extraction-policy`;
     const config = {
       method: 'PUT',
