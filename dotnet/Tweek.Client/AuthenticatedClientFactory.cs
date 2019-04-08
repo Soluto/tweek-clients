83ba14da4e0bd4e1c525652aec3af4544d0069a7
Nataly Shrits <nataly87s@gmail.com>
2019-04-08 18:48:27 +0300

    CreateAuthenticatedManagementClient

--- dotnet/Tweek.Client/AuthenticatedClientFactory.cs
+++ dotnet/Tweek.Client/AuthenticatedClientFactory.cs
@@ -9,7 +9,14 @@ namespace Tweek.Client
             AuthenticatedMessageHandler.BearerTokenProvider bearerTokenProvider)
         {
             var messageHanlder = new AuthenticatedMessageHandler(bearerTokenProvider);
-            return new TweekClient(new HttpClient(messageHanlder) { BaseAddress = baseUri });
+            return new TweekClient(new HttpClient(messageHanlder) {BaseAddress = baseUri});
+        }
+
+        public static ITweekManagementClient CreateAuthenticatedManagementClient(Uri baseUri,
+            AuthenticatedMessageHandler.BearerTokenProvider bearerTokenProvider)
+        {
+            var messageHandler = new AuthenticatedMessageHandler(bearerTokenProvider);
+            return new TweekManagementClient(new HttpClient(messageHandler) {BaseAddress = baseUri});
         }
     }
 }
