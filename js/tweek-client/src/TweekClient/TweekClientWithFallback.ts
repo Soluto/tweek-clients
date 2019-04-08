1924333a252640052039c1906fe12fbb79a3d6ad
Nataly Shrits <nataly87s@gmail.com>
2019-04-08 13:31:13 +0300

    add TweekClientWithFallback tests

--- js/tweek-client/src/TweekClient/TweekClientWithFallback.ts
+++ js/tweek-client/src/TweekClient/TweekClientWithFallback.ts
@@ -3,12 +3,17 @@ import { GetValuesConfig, ITweekClient } from './types';
 export default class TweekClientWithFallback implements ITweekClient {
   constructor(private readonly _clients: ITweekClient[]) {}
 
-  getValues<T>(path: string, config: GetValuesConfig = {}): Promise<T> {
-    return this._clients.reduce(
-      (acc, client) => acc.catch(() => client.getValues(path, config)),
-      Promise.reject() as Promise<T>,
-    );
+  getValues<T>(path: string, config: GetValuesConfig): Promise<T> {
+    return this._execute(client => client.getValues(path, config));
   }
 
-  fetch = this.getValues;
+  fetch<T>(path: string, config?: GetValuesConfig): Promise<T> {
+    return this._execute(client => client.fetch(path, config));
+  }
+
+  private _execute<T>(fn: (client: ITweekClient) => Promise<T>): Promise<T> {
+    return this._clients.reduce((acc, client) => acc.catch(() => fn(client)), Promise.reject(
+      new Error('TweekClientWithFallback has no clients'),
+    ) as Promise<T>);
+  }
 }
