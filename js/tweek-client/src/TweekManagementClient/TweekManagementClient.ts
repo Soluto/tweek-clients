c474515c1bc1e96446b701dc425e024e27a3c78c
Yshayy <yshayy@gmail.com>
2019-08-28 16:43:45 +0300

    fix

--- js/tweek-client/src/TweekManagementClient/TweekManagementClient.ts
+++ js/tweek-client/src/TweekManagementClient/TweekManagementClient.ts
@@ -105,11 +105,12 @@ export default class TweekManagementClient implements ITweekManagementClient {
     return this._fetch(url).then(toJson);
   }
 
-  search(query: string, options: number || {count?:number, type?: 'free' | 'field'} ): Promise<string[]> {
+  search(query: string, options: number | {count?:number, type?: 'free' | 'field'} = {} ): Promise<string[]> {
+    options = typeof(options) === "number" ? {count:options, type:'field'} : options;
     const queryParamsObject = {
       q: query,
-      count: options && options.count || options || defaultSearchCount,
-      type: options && options.type || 'field'
+      count: options.count || defaultSearchCount,
+      type: options.type || 'field'
     };
     const queryString = toQueryString(queryParamsObject);
     const url = `${this.config.baseServiceUrl}/api/v2/search${queryString}`;
